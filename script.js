2cc8cc0d919b05489657aefecd7b9423f606d07b
neeneemi <neeneemille@gmail.com>
2022-12-01 19:14:42 -0600

    feat: bruh

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const form = document.getElementById("form");
 const result = document.getElementById("result");
 
 fetch("values.csv")
-    .then(res => res.text)
-    .then(text => console.log(text));
+    .then(res => console.log(res.text))
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
