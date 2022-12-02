5d3b0c2a56b615120ad0e4b5bb5f9dd3863ccbf9
neeneemi <neeneemille@gmail.com>
2022-12-01 19:25:03 -0600

    feat: ✨ work

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const form = document.getElementById("form");
 const result = document.getElementById("result");
 
 fetch("./data/array_vals.json")
-    .then(res => console.log(res.json));
+    .then(response => response.json())
+    .then(data => console.log(data));
 
 fetch("./data/hash_vals.json")
-    .then(res => console.log(res.json));
+    .then(response => response.json())
+    .then(data => console.log(data));
 
 form.addEventListener("submit", function(e) {
   e.preventDefault();
