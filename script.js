1c2329d1f28b53f42782b4ca6c74faece976d04e
neeneemi <neeneemille@gmail.com>
2022-12-01 20:20:17 -0600

    feat: ✨ added stuff

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const form = document.getElementById("form");
 const birthday = document.getElementById("bday")
 const result = document.getElementById("result");
 
+const monthsDropdown = document.getElementById("months");
+const datesDropdown = document.getElementById("dates");
+
+const monthsArray = Array(12);
+const datesArray = Array(31);
+
+monthsArray.forEach((month, index) => {
+  let monthName = dayjs().month(index).format("MMMM");
+  let opt = document.createElement("option");
+  opt.value = index;
+  opt.append(monthName);
+  monthsDropdown.append(opt);
+})
+
+datesArray.forEach((date, index) => {
+  let opt = document.createElement("option");
+  opt.value = index + 1;
+  datesDropdown.append(opt);
+})
+
 fetch("./data/array_vals.json")
     .then(response => response.json())
     .then(data => console.log(data));
@@ -14,4 +34,5 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   let bdayValue = birthday.value;
+  console.log(bdayValue);
 })
