c0fc012855e6477a1c5b073a323c409aad4e7405
neeneemi <neeneemille@gmail.com>
2022-12-02 17:15:05 -0600

    refactor: 💄 adjusted wording for messages

--- script.js
+++ script.js
@@ -43,15 +43,15 @@ form.onsubmit = function(e) {
         let rankResult = dataResult.rank;
         result.innerHTML = `<p class="mainResult">Your lucky ranking is <strong>#${rankResult}</strong>!</p>`;
         if (1 < rankResult && rankResult <= 50) {
-          result.innerHTML += "<p style='font-style: italic'>Wow, it's really your lucky year! Maybe you'll find a dollar on the ground.</p>";
+          result.innerHTML += "<p style='font-style: italic'>Wow, 2023 is really gonna be your year! Maybe you'll find a dollar on the ground.</p>";
         } else if (rankResult === 1) {
           result.innerHTML += "<p style='font-style: italic'>Today is my friend Izzy's birthday so they need to be the luckiest person ever for their special day :).</p>";
         }else if (50 < rankResult && rankResult <= 100) {
-          result.innerHTML += "<p style='font-style: italic'>You're gonna experience some good luck, yippee!</p>";
+          result.innerHTML += "<p style='font-style: italic'>You're gonna experience some good luck in 2023, yippee!</p>";
         } else if (100 < rankResult && rankResult <= 200) {
-          result.innerHTML += "<p style='font-style: italic'>Your luck is pretty average this year. Don't expect anything grand to happen, okay?</p>";
+          result.innerHTML += "<p style='font-style: italic'>Your luck will be pretty average in 2023. Don't expect anything grand to happen, okay?</p>";
         } else if (200 < rankResult && rankResult <= 300) {
-          result.innerHTML += "<p style='font-style: italic'>Your luck's not gonna be the best, but keep your head up! This doesn't mean everything.</p>";
+          result.innerHTML += "<p style='font-style: italic'>Your luck's not gonna be the best in 2023, but keep your head up! This doesn't determine everything.</p>";
         } else if (300 < rankResult && rankResult <= 366) {
           result.innerHTML += "<p style='font-style: italic'>Um.</p> <img src='./images/MakoCringe.png' alt='Um.' />";
         } else {
