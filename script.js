1d1a240ab8b4ddc6634221317f1b2b479cb09925
neeneemi <neeneemille@gmail.com>
2022-12-01 20:28:05 -0600

    feat: ✨ script updates

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const result = document.getElementById("result");
 
 const monthsDropdown = document.getElementById("months");
 const datesDropdown = document.getElementById("dates");
+const monthsDatalist = document.getElementById("monthInput");
+const datesDatalist = document.getElementById("dateInput");
 
 const monthsArray = Array(12);
 const datesArray = Array(31);
 
 monthsArray.forEach((month, index) => {
+  console.log("month", month);
   let monthName = dayjs().month(index).format("MMMM");
   let opt = document.createElement("option");
   opt.value = index;
@@ -17,22 +20,20 @@ monthsArray.forEach((month, index) => {
 })
 
 datesArray.forEach((date, index) => {
+  console.log("date", date);
   let opt = document.createElement("option");
   opt.value = index + 1;
   datesDropdown.append(opt);
 })
 
-fetch("./data/array_vals.json")
-    .then(response => response.json())
-    .then(data => console.log(data));
-
-fetch("./data/hash_vals.json")
-    .then(response => response.json())
-    .then(data => console.log(data));
-
 form.addEventListener("submit", function(e) {
   e.preventDefault();
 
-  let bdayValue = birthday.value;
-  console.log(bdayValue);
+  let monthVal = monthsDropdown.value;
+  let dateVal = datesDropdown.value;
+  console.log(monthVal, dateVal);
+  
+  fetch("./data/array_vals.json")
+    .then(response => response.json())
+    .then(data => console.log(data));
 })
